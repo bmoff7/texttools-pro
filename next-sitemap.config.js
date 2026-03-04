1f86f1287840c2cc4d3a718e828aef291b548f8a
bmoff7 <bmoff7@gmail.com>
2026-03-04 00:55:26 -0500

    Add Google verification file and fix sitemap domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://texttools-pro.vercel.app',
+  siteUrl: process.env.SITE_URL || 'https://texttools-pro-alpha.vercel.app',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
